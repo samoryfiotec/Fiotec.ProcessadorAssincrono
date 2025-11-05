5fe03b8b546071173c1dbed4cf9ab6d526cc461e
Samory Pereira <samorypereira@fiotec.fiocruz.br>
2025-11-05 09:50:26 -0300

    Iniciando a implementação do UnitOfWork

---
+++ src/Fiotec.ProcessadorAssincrono.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiotec.ProcessadorAssincrono.Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //IClasseRepository Classes { get; }
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
+}
---
+++ src/Fiotec.ProcessadorAssincrono.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Fiotec.ProcessadorAssincrono.Application.Interfaces;
+
+namespace Fiotec.ProcessadorAssincrono.Infrastructure.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public Task CommitAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RollbackAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
